7d6cf96e7c4353e42ed5a26ae2e51ce7184b2471
aurbainsky <aurbainsky@gmail.com>
2017-08-17 22:01:25 -0300

    AU - Starting Proyect

---
+++ src/main/java/com/alexis/RestJerseyMongo/RestJerseyMongoApplication.java
@@ -0,0 +1,12 @@
+package com.alexis.RestJerseyMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJerseyMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestJerseyMongoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alexis/RestJerseyMongo/RestJerseyMongoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alexis.RestJerseyMongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestJerseyMongoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
